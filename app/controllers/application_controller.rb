39b89f5bec31abb67ae31364984888edf60fa669
Alexander Spanos <alexisspa9@gmail.com>
2018-04-02 17:58:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,9 +68,10 @@ end
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      
+
       erb :'tweets/edit_tweet'
     else
+      redirect to '/login'
     end
   end
   post '/tweets/:id' do
