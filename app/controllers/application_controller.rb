5a23d948d34c4cd3b2f923c771eb5c5cfacc390d
Alexander Spanos <alexisspa9@gmail.com>
2018-04-02 18:14:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -98,7 +98,7 @@ end
     end
 
   end
-  delete 'tweets/:id/delete' do
+  delete '/tweets/:id/delete' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet && @tweet.user == current_user
