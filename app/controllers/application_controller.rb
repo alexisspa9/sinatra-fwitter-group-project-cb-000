6f318f9d714d152de7fcd85cbfae91ab15c78f86
Alexander Spanos <alexisspa9@gmail.com>
2018-04-02 17:47:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,19 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
+  
   get '/tweets/new' do
     erb :create_tweet
   end
